278de42fff0cca2f565e3c2e55fe63a479f00b54
stefan.pavlovic <spstefan5@gmail.com>
2018-05-29 22:06:56 +0200

    Add project files.

---
+++ TechnologyBlogSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechnologyBlogSolution.Startup))]
+namespace TechnologyBlogSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
